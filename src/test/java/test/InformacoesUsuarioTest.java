3a5c39e749fe8bfb0adce038b501da6f8b71fbd4
Katia Xavier <sys1@grupopmz.com.br>
2019-08-12 10:26:58 -0400

    gerando evidências de testes com scrrenshots

--- src/test/java/test/InformacoesUsuarioTest.java
+++ src/test/java/test/InformacoesUsuarioTest.java
@@ -4,19 +4,34 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.concurrent.TimeUnit;
 
+import org.easetech.easytest.annotation.DataLoader;
+import org.easetech.easytest.annotation.Param;
+import org.easetech.easytest.runner.DataDrivenTestRunner;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import suporte.Generator;
+import suporte.Screenshot;
+
+@RunWith(DataDrivenTestRunner.class)
+@DataLoader(filePaths = "InformacoesUsuarioTest.csv")
 public class InformacoesUsuarioTest {
 
 	private WebDriver navegador;
+	
+	@Rule
+	public TestName test = new TestName();
 
 	@Before
 	public void setUp() {
@@ -53,8 +68,8 @@ public class InformacoesUsuarioTest {
 
 	}
 
-	// @Test Teste comentado temporariamente!
-	public void testAdicionarUmaInformacaoAdicionalDoUsuario() {
+	@Test 
+	public void testAdicionarUmaInformacaoAdicionalDoUsuario(@Param(name= "tipo")String tipo, @Param(name= "contato")String contato, @Param(name= "mensagem")String mensagemEsperada) {
 
 		// Clicar no botão através de seu spath //button[@data-target="addmoredata"]
 		navegador.findElement(By.xpath("//button[@data-target=\"addmoredata\"]")).click();
@@ -64,20 +79,21 @@ public class InformacoesUsuarioTest {
 
 		// Na combo de name "type" escolhe a opção "Phone"
 		WebElement campoType = popupAddMoreData.findElement(By.name("type"));
+		new Select(campoType).selectByVisibleText(tipo);
 
 		// No campo de name "contact" digitar "+551199999999"
-		popupAddMoreData.findElement(By.name("contact")).sendKeys("+551199999999");
+		popupAddMoreData.findElement(By.name("contact")).sendKeys(contato);
 
 		// Clicar no link de text "SAVE" que está na popup
 		popupAddMoreData.findElement(By.linkText("SAVE")).click();
 
 		// Na mensagem de id "toast-container" validar que o texto é "Your contact has bee added!"
-		WebElement mensagemPop = navegador.findElement(By.id("toast-caontainer"));
+		WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
 		String mensagem = mensagemPop.getText();
-		assertEquals("Your contact has\n bee added", mensagem);
+		assertEquals(mensagemEsperada, mensagem);
 	}
 
-	@Test
+	//@Test
 	public void removerUmContatoDeUmUsuario() {
 		//Clicar no elemento pelo seu xpath //span[text()="+5592992825179"]/following-sibling::a
 		navegador.findElement(By.xpath("//span[text()=\"+5592992825179\"]/following-sibling::a")).click();
@@ -89,6 +105,9 @@ public class InformacoesUsuarioTest {
 		WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
 		String mensagem = mensagemPop.getText();
 		assertEquals("Rest in peace, dear phone!", mensagem);
+		
+		String screenshotArquivo = "/home/sys7/Downloads/AutomacaoTeste/test-report/" + Generator.dataHoraParaArquivo() + test.getMethodName() + ".png";
+		Screenshot.tirar(navegador, screenshotArquivo );
 		
 		//Aguardar até 10 segundos para que a janela desapareça
 		WebDriverWait aguardar = new WebDriverWait(navegador,50);
